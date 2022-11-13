47c5777b880cbc33dbbcadb1566879e37a356304
Yufei Ge <gyfuiuc@gmail.com>
2022-11-13 23:19:27 +0800

    init commit

---
+++ code.h
@@ -0,0 +1,177 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// helper function for print to check the correctness
+template<typename T>
+void Print(vector<T>& arr,int size){
+    for(int i=0;i<size-1;++i){
+        cout << arr[i] << ",";
+    }
+    cout << arr[size-1] << endl;
+}
+
+// bubble sort O(n^2)
+template<typename T>
+void BubbleSortHelper(vector<T>& arr,int size){
+    for(int i=0;i<size;++i){
+        for(int j=0;j<size-i-1;++j){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+// wrapper function for BubbleSort Algorithm
+template<typename T>
+void BubbleSort(vector<T>& arr){
+    BubbleSortHelper<T>(arr,arr.size());
+}
+
+// helper function for MergeSort
+// merge from index [i,j] to [j+1,k]
+template<typename T>
+void Merge(vector<T>& arr,int i,int j,int k){
+    int size = k-i+1;
+    T temp[size];
+    int index = 0;
+    int left = i;
+    int right = j+1;
+    while(left<=j||right<=k){
+        if(left>j){
+            temp[index++] = arr[right++];
+        }else if(right>k){
+            temp[index++] = arr[left++];
+        }else{
+            if(arr[left]<=arr[right]){
+                temp[index++] = arr[left++];
+            }else{
+                temp[index++] = arr[right++];
+            }
+        }
+    }
+    for(int cnt=0;cnt<size;++cnt){
+        arr[cnt+i] = temp[cnt];
+    }
+}
+
+// merge sort O(nlogn)
+template<typename T>
+void MergeSortHelper(vector<T>& arr,int start,int end){
+    if(start>=end){
+        return;
+    }
+    int mid = start + (end-start)/2; // take care of overflow issue
+    MergeSortHelper<int>(arr,start,mid);
+    MergeSortHelper<int>(arr,mid+1,end);
+    Merge<int>(arr,start,mid,end);
+}
+
+// wrapper function for MergeSort algorithm
+template<typename T>
+void MergeSort(vector<T>& arr){
+    MergeSortHelper<T>(arr,0,arr.size()-1);
+}
+
+// helper function to randomly generate int array
+vector<int> RandomArray(){
+    srand(time(NULL));
+    int size = rand()%5000+1;
+    vector<int> arr(size,0);
+    for(int i=0;i<size;++i){
+        arr[i] = rand()%5000;
+    }
+    return arr;
+}
+
+// helper function to compare 2 arrays
+// return true if they are the same
+bool Check(vector<int>& arr1,vector<int>& arr2){
+    int size1 = arr1.size();
+    int size2 = arr2.size();
+    if(size1!=size2){
+        return false;
+    }
+    for(int i=0;i<size1;++i){
+        if(arr1[i]!=arr2[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+// helper function to test the correctness of BubbleSort algorithm
+// return false for incorrect result
+bool TestBubbleSortHelper(vector<int>& arr){
+    vector<int> temp = arr;
+    BubbleSort(temp);
+    sort(arr.begin(),arr.end());
+    return Check(arr,temp);
+}
+
+// randomly generate 1000 arrays to test the correctness of BubbleSort algorithm
+void TestBubbleSort(){
+    for(int i=0;i<1000;++i){
+        vector<int> temp = RandomArray();
+        if(!TestBubbleSortHelper(temp)){
+            cout << "BubbleSort算法的实现有误" << endl;
+        }
+    }
+    cout << "BubbleSort算法实现的正确性检测通过" << endl;
+}
+
+// helper function to test the correctness of MergeSort algorithm
+// return false for incorrect result
+bool testMergeSortHelper(vector<int>& arr){
+    vector<int> temp = arr;
+    MergeSort(arr);
+    sort(temp.begin(),temp.end());
+    return Check(arr,temp);
+}
+
+// randomly generate 1000 arrays to test the correctness of MergeSort algorithm
+void TestMergeSort(){
+    for(int i=0;i<1000;++i){
+        vector<int> temp = RandomArray();
+        if(!testMergeSortHelper(temp)){
+            cout << "MergeSort算法的实现有误" << endl;
+        }
+    }
+    cout << "MergeSort算法实现的正确性检测通过" << endl;
+}
+
+// randomly generate 1000 arrays and use both algorithms to sort them
+// calculate the total time cost and compare them
+void CompareTwoAlgorithm(){
+    vector<vector<int>> sampleArrays;
+    // randomly generate 1000 arrays, stored in sampleArrays
+    for(int i=0;i<10000;++i){
+        vector<int> temp = RandomArray();
+        sampleArrays.push_back(temp);
+    }
+    vector<vector<int>> copiedArrays = sampleArrays;
+
+    time_t startTimeBubbleSort;
+    time_t endTimeBubbleSort;
+    time_t startTimeMergeSort;
+    time_t endTimeMergeSort;
+
+    // use both algorithms to the same data set seperately
+    // and recored their start time and end time
+    time(&startTimeBubbleSort);
+    for(int i=0;i<10000;++i){
+        BubbleSort(sampleArrays[i]);
+    }
+    time(&endTimeBubbleSort);
+
+    time(&startTimeMergeSort);
+    for(int i=0;i<10000;++i){
+        MergeSort(copiedArrays[i]);
+    }
+    time(&endTimeMergeSort);
+    time_t BubbleSortCost = difftime(endTimeBubbleSort,startTimeBubbleSort);
+    time_t MergeSortCost = difftime(endTimeMergeSort,startTimeMergeSort);
+    cout << "对于相同的10000个随机数组，冒泡排序的时间花销为：" << BubbleSortCost << endl;
+    cout << "对于相同的10000个随机数组，归并排序的时间花销为：" << MergeSortCost << endl;
+}
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "code.h"
+
+
+// explicit instantiation
+template void Print<int>(vector<int>& arr,int size);
+template void BubbleSortHelper<int>(vector<int>& arr,int size);
+template void BubbleSort<int>(vector<int>& arr);
+template void Merge<int>(vector<int>& arr,int i,int j,int k);
+template void MergeSortHelper<int>(vector<int>& arr,int start,int end);
+template void MergeSort<int>(vector<int>& arr);
+
+int main(){
+    
+    // 随机生成1000个数组，数组大小为随机1至5000，数组元素为随机0至4999的int数据
+    // 先分别测试其的正确性
+    TestBubbleSort();
+    TestMergeSort();
+    
+    // 再随机生成10000个数组，数组大小为随机1至5000，数组元素为随机0至4999的int数据
+    // 拷贝一份相同的数据集
+    // 使用冒泡排序和归并排序分别对于其中一个完全相同数据集进行排序
+    // 记录下他们所花费的时间并比较
+    CompareTwoAlgorithm();
+
+    return 0;
+};
